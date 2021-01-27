d2af68090b9ffde676b85623eb102fc7d5881505
GYEISSON <yeison7f@gmail.com>
2021-01-27 15:15:20 -0500

    archivos de configuracion mvn

--- src/main/java/edu/eci/escuelaing/arep/App.java
+++ src/main/java/edu/eci/escuelaing/arep/App.java
@@ -1,5 +1,6 @@
 package edu.eci.escuelaing.arep;
 
+import static spark.Spark.*;
 /**
  * Hello world!
  *
@@ -8,6 +9,15 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+    public static  int getPort(){
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 }
