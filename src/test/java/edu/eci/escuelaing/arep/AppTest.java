5b35c6c326fa4996bab14eec1c733ebb67d62baa
GYEISSON <yeison7f@gmail.com>
2021-02-05 23:53:18 -0500

    informe

--- src/test/java/edu/eci/escuelaing/arep/AppTest.java
+++ src/test/java/edu/eci/escuelaing/arep/AppTest.java
@@ -1,20 +1,45 @@
 package edu.eci.escuelaing.arep;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	 public AppTest( String testName )
+	    {
+	        
+	        LinkedList lista = new LinkedList();
+	        lista.addFirst(159.847);
+	        lista.addFirst(0.379);lista.addFirst(753);lista.addFirst(15.5);
+	        lista.addFirst(33.5);lista.addFirst(88.78);lista.addFirst(99.0);
+	        assertEquals(99.0, ((Nodo) lista.removeFirst()).getData(),0.1);
+	        assertEquals(88.78, ((Nodo) lista.removeFirst()).getData(),0.01);
+	        assertEquals(33.5, ((Nodo) lista.removeFirst()).getData(),0.1);
+	        assertEquals(15.5, ((Nodo) lista.removeFirst()).getData(),0.1);
+	        assertEquals(753, ((Nodo) lista.removeFirst()).getData(),0.1);
+	        assertEquals(0.379, ((Nodo) lista.removeFirst()).getData(),0.001);
+	    }
+
+
+
+	    /**
+	     * Rigourous Test :-)
+	     */
+	    public void testApp()
+	    {
+	    	 LinkedList lista = new LinkedList();
+	    	 lista.addFirst(159.847);
+	         lista.addFirst(0.379);lista.addFirst(753);lista.addFirst(15.5);
+	         lista.addFirst(33.5);lista.addFirst(88.78);lista.addFirst(99.0);
+	         Calculator cal = new Calculator(lista);
+	         
+	         double mean = cal.mean();
+	         double deviation = cal.standardDeviation();
+	         
+	         assertEquals( 164.286,mean,0.001);	
+	         assertEquals(245.727,deviation, 0.001);
+	    }
 }
